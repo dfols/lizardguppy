26353fabad5c9b050ee5157697640aaa38443747
dfols <dylanjacobfoley@gmail.com>
2024-06-14 13:38:02 -0400

    reorganized project

---
+++ assets/scripts/index.js
@@ -0,0 +1,40 @@
+
+let button = document.getElementById("button");
+let audio = document.getElementById("audio");
+let image = document.getElementById("image");
+let flipInterval;
+let flipped = false;
+const interval = 375;
+
+
+function flipImage() {
+    flipped = !flipped;
+    image.style.transform = flipped ? 'scaleX(-1)' : 'scaleX(1)';
+}
+
+function toggleFlipping() {
+    if (flipInterval) {
+        clearInterval(flipInterval);
+        flipInterval = null;
+    } else {
+        flipInterval = setInterval(flipImage, interval);
+    }
+}
+
+function resetAudio() {
+    audio.currentTime = 0;
+    audio.play();
+}
+
+button.addEventListener("click", () => {
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+    toggleFlipping();
+});
+
+audio.addEventListener("ended", () => {
+    resetAudio();
+});
